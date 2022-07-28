dc33447bfbcb36f36a6f5fde1771817c7b7d1008
nektamufox <nektamy@gmail.com>
2022-07-28 19:49:38 +0300

    Update env variables + add Storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import Button from '../components/Button'
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    /* 👇 The title prop is optional.
+    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+    * to learn how to generate automatic titles
+    */
+    title: 'Button',
+    component: Button,
+  } as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+    children: 'Button',
+    className: "gradient-2 button-3 border border-design-blue w-8",
+};
