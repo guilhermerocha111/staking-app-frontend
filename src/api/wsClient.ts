7262108c0ed19cab2e226169384480eb0e9f6e2e
nektamufox <nektamy@gmail.com>
2022-09-02 22:21:55 +0300

    Update env variable

--- src/api/wsClient.ts
+++ src/api/wsClient.ts
@@ -1,3 +1,3 @@
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-export const wsClient = new W3CWebSocket('ws://localhost:8999');
+export const wsClient = new W3CWebSocket(`ws://${process.env.REACT_APP_WS_URL}:8999`);
