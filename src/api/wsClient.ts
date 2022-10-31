a8f5bda566f0ef37b3f7cdc6747b7202468fd75f
nektamufox <nektamy@gmail.com>
2022-10-31 20:49:42 +0200

    Change WSS port

--- src/api/wsClient.ts
+++ src/api/wsClient.ts
@@ -1,3 +1,3 @@
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-export const wsClient = new W3CWebSocket(`wss://${process.env.REACT_APP_WS_URL}:8999`);
+export const wsClient = new W3CWebSocket(`wss://${process.env.REACT_APP_WS_URL}:8443/`);
